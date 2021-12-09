abdaaa5ba1f1d4ac78d2bca38bba7c1e4ed4b56c
slava bily <slavabily@me.com>
2021-12-09 09:20:51 +0200

    the editor commands for addition and deletion of chars are implemented

--- SimpleTextEditor/main.swift
+++ SimpleTextEditor/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+var S = ""
+var Q = 0
+while Q < 3 {
+    
+    print("Please enter operation:")
+    
+    if let operation = readLine() {
+        
+        let pref = operation.prefix(1)
+        
+        if  pref == "1" {
+            var suff = operation.suffix(from: pref.endIndex)
+            suff.removeFirst()
+            let s = String(suff)
+            S.append(s)
+            print(S)
+        }
+        if pref == "2" {
+            S.removeLast(2)
+            print(S)
+        }
+    }
+    Q += 1
+}
+
+
+
+ 
+
 
