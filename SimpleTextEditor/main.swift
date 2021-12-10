f18b7f94667fbe8ecebecaa4c558bc66eca78a3d
slava bily <slavabily@me.com>
2021-12-10 22:23:19 +0200

    the draft of editor model is composed

--- SimpleTextEditor/main.swift
+++ SimpleTextEditor/main.swift
@@ -11,62 +11,67 @@ var S = ""
 var Q = 0
 var U = [String]()
 
-while Q < 5 {
+print("Please, enter the number of operations:")
+
+if let Q = readLine() {
+    let q = Int(Q)!
+    var n = 0
     
-    print("Please enter operation:")
-    
-    if var operation = readLine() {
+    while n < q {
+        print("Please enter operation:")
         
-        var pref = operation.prefix(1)
-        
-        var s: String.SubSequence {
-            var suff = operation.suffix(from: pref.endIndex)
-            suff.removeFirst()
-            return suff
-        }
-        if  pref == "1" {
-            U.append(operation)
-            let st = String(s)
-            S.append(st)
-            print(S)
-        }
-        if pref == "2" {
-            U.append(operation)
-            S.removeLast(Int(s)!)
-            print(S)
-        }
-        if pref == "3" {
-            guard let k = Int(s) else { print("Incorrect command"); break }
-            let arr = Array(S)
-            for i in 0..<arr.count {
-               if i == k - 1 {
-                print(arr[i])
-               }
+        if var operation = readLine() {
+            
+            var pref = operation.prefix(1)
+            
+            var s: String.SubSequence {
+                var suff = operation.suffix(from: pref.endIndex)
+                suff.removeFirst()
+                return suff
             }
-        }
-        if pref == "4" {
-            print("Undo...")
-            U.removeLast()
-            S.removeAll()
-            if U.isEmpty == false {
-                for i in U {
-                    operation = i
-                    pref = operation.prefix(1)
-                    if  pref == "1" {
-                        let st = String(s)
-                        S.append(st)
-                        print(S)
-                    } else if pref == "2" {
-                        S.removeLast(Int(s)!)
-                        print(S)
+            if  pref == "1" {
+                U.append(operation)
+                let st = String(s)
+                S.append(st)
+                print(S)
+            }
+            if pref == "2" {
+                U.append(operation)
+                S.removeLast(Int(s)!)
+                print(S)
+            }
+            if pref == "3" {
+                guard let k = Int(s) else { print("Incorrect command"); break }
+                let arr = Array(S)
+                for i in 0..<arr.count {
+                   if i == k - 1 {
+                    print(arr[i])
+                   }
+                }
+            }
+            if pref == "4" {
+                print("Undo...")
+                U.removeLast()
+                S.removeAll()
+                if U.isEmpty == false {
+                    for i in U {
+                        operation = i
+                        pref = operation.prefix(1)
+                        if  pref == "1" {
+                            let st = String(s)
+                            S.append(st)
+                            print(S)
+                        } else if pref == "2" {
+                            S.removeLast(Int(s)!)
+                            print(S)
+                        }
                     }
                 }
             }
         }
+        n += 1
     }
-    Q += 1
 }
-print(S)
 print(U)
 
 
